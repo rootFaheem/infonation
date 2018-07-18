8ffa56d9cd6e74bbe662603b7afcfcf5a305b8d3
FaheemTech <faheemtech0@gmail.com>
2018-07-18 12:04:00 +0530

    all files

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Infonation ',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Infoantion'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(2.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add more photos'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/tyre1.jpg'),
+                    Text('this is Mr. Tyre. He lives in Kota')
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
